124503134daed92f4047a64b49f2c7e2cf3da89c
381351 <demar4981@oslomet.no>
2024-01-26 15:00:27 +0100

    first commit

---
+++ src/main/java/com/example/forelesningtest/ForelesningtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.forelesningtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForelesningtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForelesningtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/forelesningtest/ForelesningtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forelesningtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForelesningtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
